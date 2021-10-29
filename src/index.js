5f89f9fb19b025accc9635610d7bae4fa74fbf1b
jst Maybe <luannguyen210500@gmail.com>
2021-10-30 01:03:51 +0700

    remove /class part

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:5000/";
-axios.defaults.baseURL = "https://tranquil-dawn-64742.herokuapp.com/class";
+axios.defaults.baseURL = "https://tranquil-dawn-64742.herokuapp.com";
 
 // axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
 axios.defaults.headers.post["Content-Type"] = "application/json";
